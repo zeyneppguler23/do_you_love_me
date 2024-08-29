57555d3fceb4553d55cc5a24488456bec3efebfd
zeyneppguler23 <157039060+zeyneppguler23@users.noreply.github.com>
2024-08-29 23:56:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const slider = document.getElementById('loveSlider');
+const gif = document.getElementById('loveGif');
 
+slider.addEventListener('input', function() {
+    const gifNumber = slider.value;
+    gif.src = `assets/gifs/gif${gifNumber}.gif`;
+});
